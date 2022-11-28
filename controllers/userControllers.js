dce6234a2e41ffad39a322ca8f2d175f03ca4f3e
Ozan Guner <67223977+ozngnr@users.noreply.github.com>
2022-11-28 06:34:34 +0000

    user controller update

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,9 +4,9 @@ const bcrypt = require('bcrypt');
 
 const validateUser = (user) => {
   const schema = Joi.object({
-    name: Joi.string().min(5).max(50).required(),
     email: Joi.string().email().min(5).max(255).trim().required(),
     password: Joi.string().min(8).max(255).trim().required(),
+    name: Joi.string().min(5).max(50).required(),
   });
 
   return schema.validate(user);
@@ -22,10 +22,7 @@ const createUser = async (req, res) => {
   if (error) return res.status(400).send(error.message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (user)
-    return res
-      .status(400)
-      .send('The email address is already associated with another account.');
+  if (user) return res.status(400).send('User already registered.');
 
   // Hash user password
   const salt = await bcrypt.genSalt(10);
@@ -41,6 +38,7 @@ const createUser = async (req, res) => {
   await user.save();
   res
     .header('x-auth-token', token)
+    .header('access-control-expose-headers', 'x-auth-token')
     .json({ _id: user._id, name: user.name, email: user.email });
 };
 
