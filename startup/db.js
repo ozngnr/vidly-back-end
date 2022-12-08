df54c9ecda3a390c4cf21d2027610ffcc313a7a9
ozngnr <ozngnr@gmail.com>
2022-12-08 13:00:49 +0000

    changed db connection function to async

--- startup/db.js
+++ startup/db.js
@@ -2,9 +2,7 @@ const logger = require('../utils/logger');
 const config = require('config');
 const mongoose = require('mongoose');
 
-module.exports = () => {
-  // const env = process.env.NODE_ENV.toUpperCase();
-  // const dbUri = process.env[`DB_${env}`];
+module.exports = async () => {
   const db = config.get('db');
-  mongoose.connect(db).then(() => logger.info(`Connected to ${db}...`));
+  await mongoose.connect(db).then(() => logger.info(`Connected to ${db}...`));
 };
