065b5e15a6399b6a50e792d1811f9f4c5d7e8614
ozngnr <ozngnr@gmail.com>
2022-12-10 17:29:25 +0000

    added cors options

--- startup/cors.js
+++ startup/cors.js
@@ -1,5 +1,10 @@
 const cors = require('cors');
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 module.exports = (app) => {
-  app.use(cors());
+  app.use(cors(corsOptions));
 };
