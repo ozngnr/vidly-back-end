d0f943f64e3b346dcaa8dd19ecf191afcb86dc39
ozngnr <ozngnr@gmail.com>
2022-12-10 18:16:48 +0000

    removed cors options

--- startup/cors.js
+++ startup/cors.js
@@ -1,10 +1,5 @@
 const cors = require('cors');
-const corsOptions = {
-  origin: '*',
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
 
 module.exports = (app) => {
-  app.use(cors(corsOptions));
+  app.use(cors());
 };
